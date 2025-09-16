4677a70bb43a010f5c4ebee03df66cfce82a784e
sagar-bhalerao-01 <s84122595@gmail.com>
2025-09-16 18:28:30 +0000

    updated

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-function validateForm() {
-    let valid = true;
-    document.getElementById('ageError').innerText= '';
-}
+document.getElementById('form').addEventListener('submit', function(event) {
+    event.priventDefault();
+    const age = document.getElementById("age").value;
+    const email = document.getElementById("email").value;
+    const mailRGX = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/;
+    if (age <= 19) {
+        alert("You must be older than 19 years old.");
+        return;
+    }
+    if (!mailRGX.test(email)) {
+        alert("Please enter the valid email ID.");
+        return;
+    }
+    alert("Form submitted...")
+})
