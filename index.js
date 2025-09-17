d412a226f01f336daf9a635eca8cca0943960e9c
sagar-bhalerao-01 <s84122595@gmail.com>
2025-09-17 05:08:05 +0000

    updated

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ document.getElementById('form').addEventListener('submit', function(event) {
     const email = document.getElementById("email").value;
     const mailRGX = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/;
     if (age < 19) {
-        Swal.fire("You must be older than 19 years old.");
+        alert("You must be older than 19 years old.");
         return;
     }
     if (!mailRGX.test(email)) {
-        Swal.fire("Please enter the valid email ID.");
+        alert("Please enter the valid email ID.");
         return;
     }
-    Swal.fire("Form submitted...");
+    alert("Form submitted...");
 })
