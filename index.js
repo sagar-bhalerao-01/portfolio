de32494602838eab3bb518074a051f22a71a7b12
sagar-bhalerao-01 <s84122595@gmail.com>
2025-09-17 13:16:55 +0000

    updated

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 document.getElementById("form").addEventListener("submit", function(event) {
     event.preventDefault();
-    /*const age = parseInt(document.getElementById("age").value, 10);*/
+    const age = parseInt(document.getElementById("age").value, 10);
     const endpoint = "https://script.google.com/macros/s/AKfycbyelu4plfqBBhcLHkn2UdkfyospyDk3txEd3ysz1c5UZYHGlmcNLOGGPGLprIsFtrk5wQ/exec";
-    /*const email = document.getElementById("email").value;*/
+    const email = document.getElementById("email").value;
     const form = event.target;
     const formData = new FormData(form);
     const mailRGX = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/;
-    if (formData.age < 19) {
+    if (age < 19) {
         alert("You must be older than 19 years old.");
         return;
     }
-    if (!mailRGX.test(formData.mail)) {
+    if (!mailRGX.test(email)) {
         alert("Please enter the valid email ID.");
         return;
     }
