c951b9be90c7bd866542b123787908612a4345f3
sagar-bhalerao-01 <s84122595@gmail.com>
2025-09-18 15:53:19 +0000

    updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.getElementById("form").addEventListener("submit", function(event) {
     event.preventDefault();
     /*const age = parseInt(document.getElementById("age").value, 10);*/
-    const endpoint = "https://script.google.com/macros/s/AKfycbxeiBVoBryl6TGNlJBf4V9cVn65Y8bCIBVEkuhkAh3UYLXnzh80hq_dLXYvOTskNIuCYg/exec";
+    const endpoint = "https://script.google.com/macros/s/AKfycbw6lnFTTIMrY0gexCZHU7qXVvIWqYBiT_WVa8cinLWeMtPZ6lwyWItaQZfgC5DpCuoxeQ/exec";
     /*const email = document.getElementById("email").value;*/
     const form = event.target;
     const formData = new FormData(form);
